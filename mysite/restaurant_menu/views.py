cbed51f1004e8a188a34dcf88c92c28154ee9aa4
edmond mulera <edmondmulera55@gmail.com>
2024-03-25 13:30:33 +0300

    creating html files

--- mysite/restaurant_menu/views.py
+++ mysite/restaurant_menu/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Item
 
 
 class MenuList(generic.ListView):
-    pass
+    queryset = Item.object.order_by("-date_created")
+    template_name = "index.html"
+
 
 
 class MenuItemDetail(generic.ListView):
-    pass
+    model = Item
+    template_name = "menu_item_detail.html"
+
