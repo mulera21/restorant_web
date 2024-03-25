1dad1351b0a978a2ef4bffff743d5a8cac594188
edmond mulera <edmondmulera55@gmail.com>
2024-03-25 15:42:50 +0300

    admin

--- mysite/restaurant_menu/views.py
+++ mysite/restaurant_menu/views.py
@@ -6,8 +6,8 @@ from .models import Item, MEAL_TYPE
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
-    def get_context_data(self):
-        context = {}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["meals"] = MEAL_TYPE
         return context
         
