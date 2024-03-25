3fcca8674c56164fc4d891a115bc1a9bacda0f1f
edmond mulera <edmondmulera55@gmail.com>
2024-03-25 12:38:13 +0300

    creating models

---
+++ mysite/restaurant_menu/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salad", "Salad"),
+    ("main_dishes", "Main_dishes"),
+    ("desserts", "Desserts")
+)
+
+STATUS (
+    (0, "unavailable"),
+    (1, "available")
+)
+
+
+class Item(models.Model):
+    meal = models.CharField(max_length=10000, unique=True)
+    description = models.CharField(max_length=2000)
+    image = models.ImageField(upload_to="products/")
+    price = models.DecimalField(decimal_places=2)
+    meal_type = models.CharField(choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT())
+    status = models.IntegerField(choices=STATUS, default=0)
