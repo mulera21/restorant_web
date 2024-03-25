ddb1a18d6d7dfa5e1187289ec7c6a58b2bc95604
edmond mulera <edmondmulera55@gmail.com>
2024-03-25 12:46:12 +0300

    creating models

--- mysite/restaurant_menu/models.py
+++ mysite/restaurant_menu/models.py
@@ -8,7 +8,7 @@ MEAL_TYPE = (
     ("desserts", "Desserts")
 )
 
-STATUS (
+STATUS = (
     (0, "unavailable"),
     (1, "available")
 )
@@ -20,5 +20,10 @@ class Item(models.Model):
     image = models.ImageField(upload_to="products/")
     price = models.DecimalField(decimal_places=2)
     meal_type = models.CharField(choices=MEAL_TYPE)
-    author = models.ForeignKey(User, on_delete=models.PROTECT())
-    status = models.IntegerField(choices=STATUS, default=0)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
